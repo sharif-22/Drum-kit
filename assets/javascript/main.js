e3f3fc17b200de9321bd53c1114c9e923a8b0b69
kajasharif31 <kajasharif366@gmail.com>
2023-05-05 12:33:21 +0530

    completed logic in js

---
+++ assets/javascript/main.js
@@ -0,0 +1,57 @@
+// dom
+const drumSetEl = document.querySelector(".drum-set");
+const drumSetElChild = drumSetEl.children;
+
+let drumTypes = [
+  { code: "KeyW", drumtype: "tom-1" },
+  { code: "KeyA", drumtype: "tom-2" },
+  { code: "KeyS", drumtype: "tom-3" },
+  { code: "KeyD", drumtype: "tom-4" },
+  { code: "KeyJ", drumtype: "snare" },
+  { code: "KeyK", drumtype: "kick-bass" },
+  { code: "KeyL", drumtype: "crash" },
+];
+
+// utillity function
+function playDrum(file) {
+  let audio = new Audio(`./assets/sounds/${file}.mp3`);
+  audio.play();
+}
+
+// setting background image by programmatically
+
+drumTypes.forEach((element, index) => {
+  // console.log(element.drumtype, index);
+  let style = drumSetElChild[index].style;
+  style.backgroundImage = `url(./assets/images/${element.drumtype}.png)`;
+});
+
+// events
+
+// click event
+drumSetEl.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.tagName === "DIV") return;
+
+  let soundFile = target.name;
+  console.log("file name and name of el in html  ", soundFile);
+
+  drumTypes.findIndex((element) => {
+    // console.log(element.drumtype);
+    if (soundFile === element.drumtype) {
+      playDrum(element.drumtype);
+    }
+  });
+});
+
+// keyboard event
+document.body.addEventListener("keydown", (e) => {
+  let code = e.code;
+  console.log(code);
+  drumTypes.findIndex((element) => {
+    // console.log(element.code);
+    if (code === element.code) {
+      playDrum(element.drumtype);
+    }
+  });
+});
